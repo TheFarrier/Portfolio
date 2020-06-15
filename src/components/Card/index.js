d7b38fa64220674e4b26866fd152e12e916da9a8
The Farrier <nkfarrey@gmail.com>
2020-06-15 18:44:02 -0500

    created separate about card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,14 +1,31 @@
 import React from "react";
 import "./style.css"
 
+export function AboutCard(props) {
+  return (
+    <div className={props.type}  className="row justify-content-center no-gutters" >
+      <div className="col-sm-12">
+        <div className="card about">
+          <div className="card-body">
+            <h1 className="heading">{props.title}</h1>
+            <div className="row no-gutters">
+              {props.children}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export function Card(props) {
   return (
-    <div class="row justify-content-center no-gutters">
-      <div class="col-sm-12">
-        <div class="card">
-          <div class="card-body">
+    <div className={props.type}  className="row justify-content-center no-gutters" >
+      <div className="col-sm-12">
+        <div className="card">
+          <div className="card-body">
             <h1 className="heading">{props.title}</h1>
-            <div class="row no-gutters">
+            <div className="row no-gutters">
               {props.children}
             </div>
           </div>
