909e4cb3aee10d270cdc78d92f00a7acbfb3ab88
The Farrier <nkfarrey@gmail.com>
2020-06-15 23:51:19 -0500

    added link to portfolio spot in page

--- src/components/Collapse/index.js
+++ src/components/Collapse/index.js
@@ -9,7 +9,7 @@ export function Collapse() {
           <a className="nav-link" href="#">About <span className="sr-only">(current)</span></a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="portfolio.html">Portfolio</a>
+          <a className="nav-link" href="#portfolio-start">Portfolio</a>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="https://docs.google.com/document/d/1XuX-_r5w-MqJv6IE4cVZ1VSU8jgMt0WAyGN6RGIJM8I/edit?usp=sharing">Resume</a>
