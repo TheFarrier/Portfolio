3cda77a397d026dfa759f5f8da3c6446af680351
The Farrier <nkfarrey@gmail.com>
2020-06-15 23:51:01 -0500

    added aditional projects to portfolio

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -7,38 +7,59 @@ import { BigProjectCard, SmallProjectCard } from "../ProjectCard";
 class Projects extends Component {
   render () {
     return (
-    <Card title={"Projects"}>
-      <BigProjectCard 
-        href = {"https://gif-lash.herokuapp.com/"}
-        image = {require('../../images/giflash.jpg')}
-        title = {"Gif-Lash Multiplayer Game"}
-        github = {"https://github.com/TheFarrier/Gif-Lash"}
-      />
-      <BigProjectCard 
-        href = {"https://thefarrier.github.io/bored-project/"}
-        image = {require('../../images/bored.jpg')}
-        title = {"Neverbored"}
-        github = {"https://github.com/TheFarrier/Never-Bored"}
-      />
-      <SmallProjectCard 
-        href = {"https://thefarrier.github.io/Homework3/Develop/index.html"}
-        image = {require('../../images/password.jpg')}
-        title = {"Password Generator"}
-        github = {"https://github.com/TheFarrier/TheFarrier.github.io/tree/master/Homework3"}
-      />
-      <SmallProjectCard 
-        href = {"https://thefarrier.github.io/Homework4/Develop/index.html"}
-        image = {require('../../images/quiz.gif')}
-        title = {"Quiz App"}
-        github = {"https://github.com/TheFarrier/TheFarrier.github.io/tree/master/Homework4"}
-      />
-      <SmallProjectCard 
-        href = {"https://thefarrier.github.io/Homework6/Develop/index.html"}
-        image = {require('../../images/weather.jpg')}
-        title = {"Weather App"}
-        github = {"https://github.com/TheFarrier/TheFarrier.github.io/tree/master/Homework6"}
-      />
-    </Card>
+      <a id="portfolio-start">
+        <Card title={"Projects"}>
+          <BigProjectCard 
+            href = {"https://gif-lash.herokuapp.com/"}
+            image = {require('../../images/giflash.jpg')}
+            title = {"Gif-Lash Multiplayer Game"}
+            github = {"https://github.com/TheFarrier/Gif-Lash"}
+          />
+          <BigProjectCard 
+            href = {"https://thefarrier.github.io/bored-project/"}
+            image = {require('../../images/bored.jpg')}
+            title = {"Neverbored"}
+            github = {"https://github.com/TheFarrier/Never-Bored"}
+          />
+          <SmallProjectCard 
+            href = {"https://tranquil-oasis-71844.herokuapp.com/?id=5ecf13ddfbda0800175ba213"}
+            image = {require('../../images/workout.jpg')}
+            title = {"Workout Tracker"}
+            github = {"https://github.com/TheFarrier/Workout-Tracker"}
+          />
+          <SmallProjectCard 
+            href = {"https://thefarrier.github.io/Homework5/Develop/index.html"}
+            image = {require('../../images/Scheduler.jpg')}
+            title = {"Daily Scheduler"}
+            github = {"https://github.com/TheFarrier/TheFarrier.github.io/tree/master/Homework5"}
+          />
+          <SmallProjectCard 
+            href = {"https://note-taker-8754.herokuapp.com/"}
+            image = {require('../../images/notetaker.jpg')}
+            title = {"Note Taker"}
+            github = {"https://github.com/TheFarrier/TheFarrier.github.io/tree/master/Homework11"}
+          />
+          <SmallProjectCard 
+            href = {"https://thefarrier.github.io/Homework3/Develop/index.html"}
+            image = {require('../../images/password.jpg')}
+            title = {"Password Generator"}
+            github = {"https://github.com/TheFarrier/TheFarrier.github.io/tree/master/Homework3"}
+          />
+          <SmallProjectCard 
+            href = {"https://thefarrier.github.io/Homework4/Develop/index.html"}
+            image = {require('../../images/quiz.gif')}
+            title = {"Quiz App"}
+            github = {"https://github.com/TheFarrier/TheFarrier.github.io/tree/master/Homework4"}
+          />
+          <SmallProjectCard 
+            href = {"https://thefarrier.github.io/Homework6/Develop/index.html"}
+            image = {require('../../images/weather.jpg')}
+            title = {"Weather App"}
+            github = {"https://github.com/TheFarrier/TheFarrier.github.io/tree/master/Homework6"}
+          />
+        </Card>
+      </a>
+    
   );
 }
   
