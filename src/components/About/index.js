223860003dc2ffbddb446911d46b22a3cccf22ec
The Farrier <nkfarrey@gmail.com>
2020-07-16 03:49:04 -0500

    changed about paragraph

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -11,7 +11,7 @@ function About() {
       <div className="col-sm-9 xs-12">
         <div className="card-body text-center align-middle pt-5 mt-5">
           <h1>Nathaniel Farrey</h1>
-          <p className="card-text">Nathaniel Farrey is a web developer, musician, and dog lover. He is experienced with HTML5, CSS, and Javascript and is learning full stack development using the MERN stack at The UTSA School of Data Science. </p>
+          <p className="card-text">Nathaniel Farrey is a web developer, musical hobbyist, and dog lover. He recieved a Certificate in Full Stack Web Development from The UTSA School of Data Science, where he studied the fundamentals of web development and the MERN stack.  </p>
         </div>
       </div>
     </AboutCard>
